47fec6edbc8957077d49f2bf02e58aea03dd9e38
dimchee <dimchee90@gmail.com>
2024-08-07 20:04:29 +0200

    R U Ready?

---
+++ Readme.md
@@ -0,0 +1,1 @@
+# Zorsh - simple game
---
+++ build.zig
@@ -0,0 +1,62 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const exe = b.addExecutable(.{
+        .name = "zorsh",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(exe);
+    buildRun(b, exe);
+    buildTest(b, target, optimize);
+    buildRaylib(b, target, exe, optimize);
+}
+
+pub fn buildRun(b: *std.Build, exe: *std.Build.Step.Compile) void {
+    const run_cmd = b.addRunArtifact(exe);
+    // if the application depends on other installed files, this ensures they will be present and in the expected location.
+    run_cmd.step.dependOn(b.getInstallStep());
+    // This allows the user to pass arguments to the application in the build command itself, like this: `zig build run -- arg1 arg2 etc`
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
+
+pub fn buildTest(
+    b: *std.Build,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+) void {
+    const unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&b.addRunArtifact(unit_tests).step);
+}
+
+pub fn buildRaylib(
+    b: *std.Build,
+    target: std.Build.ResolvedTarget,
+    exe: *std.Build.Step.Compile,
+    optimize: std.builtin.OptimizeMode,
+) void {
+    const raylib_dep = b.dependency("raylib-zig", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const raylib = raylib_dep.module("raylib"); // main raylib module
+    const raygui = raylib_dep.module("raygui"); // raygui module
+    const raylib_artifact = raylib_dep.artifact("raylib"); // raylib C library
+    raylib_artifact.defineCMacro("USE_EXTERNAL_GLFW", null);
+    exe.linkLibrary(raylib_artifact);
+    exe.root_module.addImport("raylib", raylib);
+    exe.root_module.addImport("raygui", raygui);
+}
