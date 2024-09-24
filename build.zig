b50e73c7981b2199dddc7e4616dc95c6ec3abfd8
dimchee <dimchee90@gmail.com>
2024-09-24 22:22:03 +0200

    Simplify build.zig

--- build.zig
+++ build.zig
@@ -13,43 +13,18 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("assets/map"),
     });
     b.installArtifact(exe);
-    buildRun(b, exe);
-    buildTest(b, target, optimize);
-    buildRaylib(b, target, exe, optimize);
-}
-
-pub fn buildRun(b: *std.Build, exe: *std.Build.Step.Compile) void {
     const run_cmd = b.addRunArtifact(exe);
-    // if the application depends on other installed files, this ensures they will be present and in the expected location.
     run_cmd.step.dependOn(b.getInstallStep());
-    // This allows the user to pass arguments to the application in the build command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-}
+    run_cmd.addArgs(b.args orelse &.{});
 
-pub fn buildTest(
-    b: *std.Build,
-    target: std.Build.ResolvedTarget,
-    optimize: std.builtin.OptimizeMode,
-) void {
     const unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&b.addRunArtifact(unit_tests).step);
-}
+    b.step("run", "Run the app").dependOn(&run_cmd.step);
+    b.step("test", "Run unit tests").dependOn(&b.addRunArtifact(unit_tests).step);
 
-pub fn buildRaylib(
-    b: *std.Build,
-    target: std.Build.ResolvedTarget,
-    exe: *std.Build.Step.Compile,
-    optimize: std.builtin.OptimizeMode,
-) void {
     const raylib_dep = b.dependency("raylib-zig", .{
         .target = target,
         .optimize = optimize,
